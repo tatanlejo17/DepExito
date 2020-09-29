e1656ccaed35808519c753a7520809b8aa2d3dce
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-29 15:44:51 +0000

    Deploy tatanlejo17/DepExito to github.com/tatanlejo17/DepExito.git:gh-pages

---
+++ precache-manifest.22c9fc79116bcb4d1402e8bea8e0145f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f86a2895751ff0262d4e0c021b2596",
+    "url": "/DepExito/index.html"
+  },
+  {
+    "revision": "00be923fe704c06038a4",
+    "url": "/DepExito/static/css/main.ac75db91.chunk.css"
+  },
+  {
+    "revision": "6aa3d5cf440d3db7e54a",
+    "url": "/DepExito/static/js/2.2f32d70a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DepExito/static/js/2.2f32d70a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00be923fe704c06038a4",
+    "url": "/DepExito/static/js/main.1d0c3838.chunk.js"
+  },
+  {
+    "revision": "b7fd9953ddaeb8456a98",
+    "url": "/DepExito/static/js/runtime-main.43537a0d.js"
+  }
+]);
